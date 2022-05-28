009d408d7c0bde5267da6341a46c1fbeef93cc57
Dan <webdevd4n@gmail.com>
2022-05-28 22:32:53 +0900

    Imported Circle component

--- src/components/organisms/countdownSection/countdownSection.jsx
+++ src/components/organisms/countdownSection/countdownSection.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Section } from '../../layouts/section'
 import { gradient } from '../../../design'
+import Circle from '../featuresSection/Circle' /* Change path when moved to modules folder */
 
 export const CountdownSection = React.forwardRef((props, ref) => {
-    return (
-        <Section ref={ref} background={gradient.blackGrey}>
-          <h2>Defi 3.0 Begins Here</h2>
-          <div>
-            <p>Icon</p>
-            <p>Icon</p>
-            <p>Icon</p>
+  return (
+    <Section ref={ref} background={gradient.blackGrey}>
+      <Circle />
+      <h2>Defi 3.0 Begins Here</h2>
+      <div>
+        <p>Icon</p>
+        <p>Icon</p>
+        <p>Icon</p>
 
-            <p>Timer</p>
-          </div>
-        </Section>
-    )
+        <p>Timer</p>
+      </div>
+    </Section>
+  )
 })
