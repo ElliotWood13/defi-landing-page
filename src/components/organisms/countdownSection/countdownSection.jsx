5b52f1e853b267ebd16aec22546455e98ab91b88
Elliot Wood <43952853+ElliotWood13@users.noreply.github.com>
2022-06-04 20:31:24 +0100

    Update countdown section to styled comps and without timer

--- src/components/organisms/countdownSection/countdownSection.jsx
+++ src/components/organisms/countdownSection/countdownSection.jsx
@@ -1,80 +1,39 @@
 import React from 'react'
 import { Section } from '../../layouts/section'
 import { gradient } from '../../../design'
-import Circle from '../featuresSection/Circle' /* Change path when moved to modules folder */
+import { SectionWrapper, CountdownContainer, BackgroundCircleInnerMd, BackgroundCircleOuterMd, BackgroundCircleInnerLg, BackgroundCircleOuterLg, CountdownCircle, HeaderWrapper, HeaderTop, HeaderBottom, IconsWrapper, StyledSvg, DiscordSvgWrapper, TwitterSvgWrapper,MediumSvgWrapper } from './countdownSection.styles'
 
-import { Discord } from '../../atoms/icons/discord'
-import { Twitter } from '../../atoms/icons/twitter'
-import { Medium } from '../../atoms/icons/medium'
-
-import './countdownSection.css'
-
-// TODO: Fix countdown timer shadow (currently too big)
-// TODO: Convert to components
-// ❗️ TODO: Make button icons bigger
-// ❗️ TODO: Make header text smaller on large screens
 // ❗️ TODO: Import image to go under header
 
 export const CountdownSection = React.forwardRef((props, ref) => {
   return (
     <Section ref={ref} background={gradient.greyBlack}>
-      <div className="countdown-container">
-        <div className="background-circle bg-circle--md"></div>
-        <div className="background-circle bg-circle--md-outer"></div>
-        <div className="background-circle bg-circle--lg"></div>
-        <div className="background-circle bg-circle--lg-outer"></div>
+      <SectionWrapper>
+        <CountdownContainer>
+          <BackgroundCircleInnerMd />
+          <BackgroundCircleOuterMd />
+          <BackgroundCircleInnerLg />
+          <BackgroundCircleOuterLg />
+          <CountdownCircle />
 
+          <HeaderWrapper>
+            <HeaderTop>Defi 3.0</HeaderTop>
+            <HeaderBottom>begins here</HeaderBottom>
+          </HeaderWrapper>
+        </CountdownContainer>
 
-        {/* <Circle color="yellow" /> */}
-
-
-        {/* <h2>Defi 3.0 Begins Here</h2> */}
-
-        <div className="icons">
-          <button className="icons__button btn-round">
-            <Discord />
-          </button>
-          <button className="icons__button btn-square">
-            <Twitter />
-          </button>
-          <button className="icons__button btn-transparent">
-            <Medium />
-          </button>
-        </div>
-
-        {/* <div className="circle-container"> */}
-        {/* <div className="circle"></div>
-        <h3>Defi 3.0</h3>
-        <h3>starts here</h3> */}
-        {/* </div> */}
-        <div className="countdown-circle"></div>
-
-        <div className="header">
-          <h3 className="header__top">Defi 3.0</h3>
-          <h3 className="header__bottom">begins here</h3>
-        </div>
-
-        <div className="countdown-timer">
-          <h2 className="countdown-timer__heading">Countdown Timer</h2>
-
-          <div className="countdown-timer__clock">
-            <div className="clock__row">
-              <div className="clock__row--square">3</div>
-              <div className="clock__row--square">3</div>
-            </div>
-            <div className="clock__row">
-              <div className="clock__row--square">1</div>
-              <div className="clock__row--square">1</div>
-            </div>
-            <div className="clock__row">
-              <div className="clock__row--square">4</div>
-              <div className="clock__row--square">4</div>
-            </div>
-          </div>
-        </div>
-
-
-      </div>
+        <IconsWrapper>
+            <DiscordSvgWrapper>
+              <StyledSvg type="discord" />
+            </DiscordSvgWrapper>
+            <TwitterSvgWrapper>
+              <StyledSvg type="twitter" />
+            </TwitterSvgWrapper>
+            <MediumSvgWrapper>
+              <StyledSvg type="medium" />
+            </MediumSvgWrapper>
+          </IconsWrapper>
+      </SectionWrapper>
     </Section>
   )
 })
--- src/components/organisms/countdownSection/countdownSection.styles.js
+++ src/components/organisms/countdownSection/countdownSection.styles.js
@@ -1,1 +1,249 @@
 import styled from 'styled-components'
+import { mediaQuery, pxToRem, color, fontWeight } from '../../../design'
+import { Svg } from '../../../helpers/svgMapper'
+
+const SectionWrapper = styled.div`
+  position: relative;
+`
+
+const CountdownContainer = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`
+
+const BackgroundCircleInnerMd = styled.div`
+  display: none;
+
+  ${mediaQuery.sm} {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid rgba(255, 255, 0, 0.3);
+    border-radius: 50%;
+    width: ${pxToRem(320)};
+    height: ${pxToRem(320)};
+  }
+`
+
+const BackgroundCircleOuterMd = styled.div`
+  display: none;
+
+  ${mediaQuery.sm} {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid rgba(255, 255, 0, 0.3);
+    border-radius: 50%;
+    width: ${pxToRem(340)};
+    height: ${pxToRem(340)};
+  }
+`
+
+const BackgroundCircleInnerLg = styled.div`
+  display: none;
+
+  ${mediaQuery.sm} {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid rgba(255, 255, 0, 0.3);
+    border-radius: 50%;
+    width: ${pxToRem(600)};
+    height: ${pxToRem(600)};
+  }
+`
+
+const BackgroundCircleOuterLg = styled.div`
+  display: none;
+
+  ${mediaQuery.sm} {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid rgba(255, 255, 0, 0.3);
+    border-radius: 50%;
+    width: ${pxToRem(630)};
+    height: ${pxToRem(630)};
+  }
+`
+
+const CountdownCircle = styled.div`
+  display: none;
+
+  ${mediaQuery.sm} {
+    display: block;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    width: ${pxToRem(300)};
+    height: ${pxToRem(300)};
+    border-radius: 50%;
+    border: 1px solid yellow;
+    box-shadow: 0 0 10px rgba(255, 255, 0, 0.5), 0 0 10px rgba(255, 255, 0, 0.5);
+  }
+`
+
+const HeaderWrapper = styled.div`
+  position: relative;
+  text-align: center;
+  width: 100%;
+  max-width: ${pxToRem(300)};
+  margin-bottom: ${pxToRem(50)};
+`
+
+const HeaderTop = styled.h3`
+  font-size: ${pxToRem(70)};
+  color: white;
+  text-transform: uppercase;
+  font-weight: 300;
+  text-shadow: 0 0 5px rgba(255, 255, 0, 0.3), 0 0 10px rgba(255, 255, 0, 0.3),
+    0 0 15px rgba(255, 255, 0, 0.3), 0 0 20px rgba(255, 255, 0, 0.3),
+    0 0 30px rgba(255, 255, 0, 0.3), 0 0 40px rgba(255, 255, 0, 0.3),
+    0 0 55px rgba(255, 255, 0, 0.3), 0 0 75px rgba(255, 255, 0, 0.3);
+  margin-top: ${pxToRem(30)};
+  margin-bottom: -${pxToRem(20)};
+
+  ${mediaQuery.sm} {
+    font-size: ${pxToRem(60)};
+  }
+`
+
+const HeaderBottom = styled.h3`
+  color: ${color.white[100]};
+  text-transform: uppercase;
+  font-weight: ${fontWeight.light};
+  text-shadow: 0 0 5px rgba(255, 255, 0, 0.3), 0 0 10px rgba(255, 255, 0, 0.3),
+    0 0 15px rgba(255, 255, 0, 0.3), 0 0 20px rgba(255, 255, 0, 0.3),
+    0 0 30px rgba(255, 255, 0, 0.3), 0 0 40px rgba(255, 255, 0, 0.3),
+    0 0 55px rgba(255, 255, 0, 0.3), 0 0 75px rgba(255, 255, 0, 0.3);
+  font-size: ${pxToRem(42)};
+
+  ${mediaQuery.sm} {
+    font-size: ${pxToRem(36)};
+  }
+`
+
+const IconsWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ${mediaQuery.xxs} {
+    justify-content: center;
+  }
+
+  ${mediaQuery.sm} {
+    position: absolute;
+    bottom: -${pxToRem(174)};
+    transform: translate(-50%);
+    left: 50%;
+    width: auto;
+  }
+`
+
+const StyledSvg = styled(Svg)`
+  height: ${pxToRem(30)};
+  width: ${pxToRem(30)};
+`
+
+const DiscordSvgWrapper = styled.a`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${pxToRem(80)};
+  height: ${pxToRem(80)};
+  transition: all 0.3s;
+  background-color: rgb(50, 50, 50);
+  border: none;
+  border-radius: 50%;
+  margin: ${pxToRem(8)};
+
+  :hover {
+    transform: scale(120%);
+    filter: brightness(120%);
+  }
+
+  ${mediaQuery.xxs} {
+    margin: ${pxToRem(15)};
+  }
+`
+
+const TwitterSvgWrapper = styled.a`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${pxToRem(80)};
+  height: ${pxToRem(80)};
+  transition: all 0.3s;
+  background-color: rgb(50, 50, 50);
+  border: none;
+  border-radius: 10px;
+  margin: ${pxToRem(8)};
+
+  :hover {
+    transform: scale(120%);
+    filter: brightness(120%);
+  }
+
+  ${mediaQuery.xxs} {
+    margin: ${pxToRem(15)};
+  }
+`
+
+const MediumSvgWrapper = styled.a`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: ${pxToRem(80)};
+  height: ${pxToRem(80)};
+  transition: all 0.3s;
+  background-color: rgb(50, 50, 50);
+  border: none;
+  border-radius: 50%;
+  margin: ${pxToRem(8)};
+
+  :hover {
+    transform: scale(120%);
+    filter: brightness(120%);
+  }
+
+  ${mediaQuery.xxs} {
+    margin: ${pxToRem(15)};
+  }
+`
+
+export {
+  SectionWrapper,
+  CountdownContainer,
+  BackgroundCircleInnerMd,
+  BackgroundCircleOuterMd,
+  BackgroundCircleInnerLg,
+  BackgroundCircleOuterLg,
+  CountdownCircle,
+  HeaderWrapper,
+  HeaderTop,
+  HeaderBottom,
+  IconsWrapper,
+  StyledSvg,
+  DiscordSvgWrapper,
+  TwitterSvgWrapper,
+  MediumSvgWrapper,
+}
